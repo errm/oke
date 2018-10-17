531dad85dac16b772e3328f77ce668a27b73931d
Ed Robinson <edward-robinson@cookpad.com>
2018-10-17 16:13:00 +0100

    Proto config formatting

--- lib/oke/cli.rb
+++ lib/oke/cli.rb
@@ -1,5 +1,6 @@
 require "thor"
 require "colorize"
+require "fileutils"
 
 module Oke
   class CLI < Thor
@@ -8,14 +9,46 @@ module Oke
       puts "Detected Rails Application 🛤️"
       puts ""
       puts "Preparing config files 📝"
-      create "config/oke"
-      create "config/oke/processes.rb"
-      create "config/oke/environments.rb"
+
+      create "config/oke/processes.rb", <<~HEREDOC
+        # This file contains information about every process your application needs to run
+
+        puma "app" do
+          workers 5
+          threads 5
+        end
+
+        # You can define cron jobs like this:
+        #
+        # cron "cleanup" do
+        #   schedule "@daily"
+        #   command "bundle exec rake cleanup"
+        # end
+        #
+        # cron "update counts" do
+        #   schedule "*/30 * * * *"
+        #   command "bundle exec rake counts:update"
+        # end
+        #
+        # You can manage resque workers like this :
+        #
+        # resque "wokers" do
+        #   queues %w(normal_priority low_priority default)
+        #   wokers 6
+        # end
+        #
+        # resque "high_priority" do
+        #   queues %w(high_priority)
+        #   wokers 2
+        # end
+      HEREDOC
     end
 
     no_commands do
-      def create(filename)
+      def create(filename, content)
         puts "    create ".colorize(:green) + filename
+        FileUtils.mkdir_p File.dirname(filename)
+        File.write(filename,content)
       end
     end
   end
